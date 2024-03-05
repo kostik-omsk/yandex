efb711312d324e79387363894d3eeffbea002548
Kostia S <kosiak5587@gmail.com>
2024-03-05 14:20:45 +0600

    feat: add a path to a photo

--- src/js/players.js
+++ src/js/players.js
@@ -1,38 +1,40 @@
+import img from '../assets/images/player.png';
+
 export default [
   {
     order: 1,
     name: 'Хозе-Рауль Капабланка',
     description: 'Чемпион мира по шахматам',
-    photo: './src/assets/images/player.png',
+    photo: img,
   },
   {
     order: 2,
     name: 'Эммануил Ласкер',
     description: 'Чемпион мира по шахматам',
-    photo: './src/assets/images/player.png',
+    photo: img,
   },
   {
     order: 3,
     name: 'Александр Алехин',
     description: 'Чемпион мира по шахматам',
-    photo: './src/assets/images/player.png',
+    photo: img,
   },
   {
     order: 4,
     name: 'Арон Нимцович',
     description: 'Чемпион мира по шахматам',
-    photo: './src/assets/images/player.png',
+    photo: img,
   },
   {
     order: 5,
     name: 'Рихард Рети',
     description: 'Чемпион мира по шахматам',
-    photo: './src/assets/images/player.png',
+    photo: img,
   },
   {
     order: 6,
     name: 'Остап Бендер',
     description: 'Чемпион мира по шахматам',
-    photo: './src/assets/images/player.png',
+    photo: img,
   },
 ];
